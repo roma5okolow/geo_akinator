62bee5d7bf3724afdfb596f923ddb5af57be121c
Roman Sokolov <rsokolov@avito.ru>
2025-06-11 14:35:37 +0300

    updated train scriptf

--- train.py
+++ train.py
@@ -8,10 +8,10 @@ from transformers import BertModel, BertPreTrainedModel, TrainingArguments, Trai
 
 
 # Load your CSV with 'text', 'latitude', 'longitude'
-df = pd.read_csv("gpt41_markup.csv").dropna()[['description', 'lat', 'lng', 'city']]
+df = pd.read_csv("full_markup_v2.csv")[['description', 'lat', 'lng']].dropna()
 
 # Split
-train_df, val_df = train_test_split(df, test_size=0.1, random_state=42)
+train_df, val_df = train_test_split(df, test_size=0.01, random_state=42)
 
 # Convert to HF Datasets
 train_dataset = Dataset.from_pandas(train_df.reset_index(drop=True))
@@ -22,14 +22,14 @@ tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
 def tokenize(example):
     return tokenizer(example["description"], padding="max_length", truncation=True, max_length=128)
 
-train_dataset = train_dataset.map(tokenize, batched=True)
+train_dataset = train_dataset.map(tokenize, batched=True, num_proc=20)
 val_dataset = val_dataset.map(tokenize, batched=True)
 
 def add_labels(example):
     example["labels"] = torch.tensor([example["lat"], example["lng"]], dtype=torch.float)
     return example
 
-train_dataset = train_dataset.map(add_labels)
+train_dataset = train_dataset.map(add_labels, num_proc=20)
 val_dataset = val_dataset.map(add_labels)
 
 train_dataset.set_format(type="torch", columns=["input_ids", "attention_mask", "labels"])
@@ -40,9 +40,11 @@ class BoundedLatLonRegressor(nn.Module):
     def __init__(self, input_dim):
         super().__init__()
         self.mlp = nn.Sequential(
-            nn.Linear(input_dim, 128),
+            nn.Linear(input_dim, 256),
             nn.ReLU(),
-            nn.Linear(128, 2),
+            nn.Linear(256, 64),
+            nn.ReLU(),
+            nn.Linear(64, 2),
             nn.Tanh()  # Output in [-1, 1]
         )
 
@@ -83,15 +85,16 @@ class BertForLatLonCosineLoss(BertPreTrainedModel):
 
 
 training_args = TrainingArguments(
-    output_dir="./latlon_model",
-    per_device_train_batch_size=16,
-    per_device_eval_batch_size=16,
-    eval_strategy="epoch",
-    save_strategy="epoch",
+    output_dir="./latlon_model_v5",
+    per_device_train_batch_size=128,
+    per_device_eval_batch_size=128,
+    eval_strategy="steps",
+    save_strategy="steps",
+    eval_steps = 1000,
     num_train_epochs=5,
-    weight_decay=0.01,
-    logging_dir="./logs",
-    logging_steps=10,
+    weight_decay=0.0001,
+    logging_dir="./logs/bert_v4",
+    logging_steps=50,
     report_to=["tensorboard"],
 )
 
@@ -105,4 +108,3 @@ trainer = Trainer(
 )
 
 trainer.train()
-
